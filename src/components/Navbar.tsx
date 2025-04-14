2e356fe8e80cf0b29490ef7ced529937c974af79
Hakan Gülmek <gulmekhakan6@gmail.com>
2025-04-14 13:12:19 +0300

    style: update MobileMenu and Navbar components for improved responsiveness and layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,42 +11,42 @@ import { IoPeople } from "react-icons/io5";
 import { BiSolidMessageSquareDetail } from "react-icons/bi";
 import { IoMdNotifications } from "react-icons/io";
 import { IoLogInSharp } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
 function Navbar() {
   return (
     <div className=" h-24 flex items-center justify-between">
-      <div className="hidden md:flex gap-8 items-center font-medium">
+      <div className="md:hidden lg:block w-[20%]">
         <Link href="/" className="font-bold text-xl text-blue-600">
           Social
         </Link>
       </div>
-      <div className="hidden md:flex gap-8 items-center font-medium">
-        <div className="flex gap-8 items-center md:flex ">
-          <Link
-            href="/"
-            className="hover:text-blue-600 flex items-center gap-2"
-          >
+      <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex items-center gap-2">
             <FaHome width={24} height={24} />
             Home Page
           </Link>
-          <Link
-            href="/"
-            className="hover:text-blue-600 flex items-center gap-2"
-          >
+          <Link href="/" className="flex items-center gap-2">
             <FaUserFriends width={24} height={24} />
             Friends
           </Link>
-          <Link
-            href="/"
-            className="hover:text-blue-600 flex items-center gap-2"
-          >
+          <Link href="/" className="flex items-center gap-2">
             <MdAutoStories width={24} height={24} />
             Stories
           </Link>
         </div>
+        <div className="hidden xl:flex p-2 bg-slate-100 rounded-xl items-center ">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent outline-none border-none "
+          />
+          <IoIosSearch width={14} height={14} className="" />
+        </div>
       </div>
-      <div className=" flex items-center xl:gap-8">
+      <div className=" w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
         <ClerkLoading>
-          <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white " />
+          <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
         </ClerkLoading>
         <ClerkLoaded>
           <SignedIn>
@@ -62,7 +62,7 @@ function Navbar() {
             <UserButton />
           </SignedIn>
           <SignedOut>
-            <div className="flex items-center gap-2 cursor-pointer">
+            <div className="flex items-center gap-2 text-sm">
               <IoLogInSharp width={24} height={24} />
               <Link href="/sign-in" className=" font-medium">
                 Login/Register
